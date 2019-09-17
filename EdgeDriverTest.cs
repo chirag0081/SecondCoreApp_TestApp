08d887645fec2d44d91f7f5e50cb206213310ba5
arrowchirag0081 <arrow.chirag@gmail.com>
2019-09-17 12:50:38 +0530

    Use Chrome Driver instaead Edge driver

--- EdgeDriverTest.cs
+++ EdgeDriverTest.cs
@@ -1,8 +1,10 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Support.UI;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace EdgeDriverTest1
 {
@@ -14,7 +16,7 @@ namespace EdgeDriverTest1
         // to install Microsoft WebDriver.
 
 
-        private EdgeDriver _driver;
+        private ChromeDriver _driver;
         WebDriverWait wait;
         [TestInitialize]
         public void EdgeDriverInitialize()
@@ -24,12 +26,13 @@ namespace EdgeDriverTest1
             {
                 PageLoadStrategy = PageLoadStrategy.Normal,
             };
-            _driver = new EdgeDriver(options);
+            //_driver = new EdgeDriver(options);
+            _driver = new ChromeDriver(".");
             wait = new WebDriverWait(_driver, new System.TimeSpan(0, 0, 10));
         }
 
         [TestMethod]
-        public void Login()
+        public void TestMethod_01()
         {
             _driver.Url = "http://localhost:1001/login";
             _driver.Manage().Window.Maximize();
@@ -42,24 +45,26 @@ namespace EdgeDriverTest1
             IWebElement elementNext = _driver.FindElementByClassName("btn");
             elementNext.Click();
             
-            System.Threading.Thread.Sleep(3000);
+            System.Threading.Thread.Sleep(2000);
 
             IList<IWebElement> webElementsLinks = _driver.FindElementsByClassName("nav-link");
             Assert.AreEqual(3, webElementsLinks.Count);
             Assert.AreEqual("List", webElementsLinks[0].Text);
             Assert.AreEqual("Create", webElementsLinks[1].Text);
-            Assert.AreEqual("Logout1 abc@gmail.com", webElementsLinks[2].Text);
-            System.Threading.Thread.Sleep(3000);
+            Assert.AreEqual("Logout abc@gmail.com", webElementsLinks[2].Text);
+            System.Threading.Thread.Sleep(1000);
         }
 
         [TestMethod]
-        public void VerifyPageTitle()
+       
+        public void TestMethod_02()
         {
             // Replace with your own test logic
-            _driver.Url = "http://localhost:1001";
-            System.Threading.Thread.Sleep(3000);
+            _driver.Url = "http://localhost:1001/";
+            _driver.Manage().Window.Maximize();
+            System.Threading.Thread.Sleep(1000);
             Assert.AreEqual("ClientApp", _driver.Title);
-            System.Threading.Thread.Sleep(3000);
+            
         }
 
 
@@ -67,7 +72,16 @@ namespace EdgeDriverTest1
         [TestCleanup]
         public void EdgeDriverCleanup()
         {
+            _driver.Close();
             _driver.Quit();
+            System.Threading.Thread.Sleep(500);
+            Process[] chromeDriverProcesses = Process.GetProcessesByName("chromedriver");
+
+            foreach (var chromeDriverProcess in chromeDriverProcesses)
+            {
+                chromeDriverProcess.Kill();
+            }
+           
         }
     }
 }
